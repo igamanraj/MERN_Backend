92d7026975d0b3538e1043bfa6d0ee3cd3b42b4b
igamanraj <amanra812727@gmail.com>
2025-06-17 00:00:08 +0530

    Admin and Auth Middlware setup successfully

--- routes/admin-route.js
+++ routes/admin-route.js
@@ -1,8 +1,10 @@
 const express = require("express")
-const adminController = require("../controllers/admin-controller")
 const router = express.Router()
+const adminController = require("../controllers/admin-controller")
+const authMiddleware = require("../middlewares/auth-middleware")
+const adminMiddleware = require("../middlewares/admin-middleware")
 
-router.route("/users").get(adminController.getAllUsers);
-router.route("/contacts").get(adminController.getAllContacts);
+router.route("/users").get(authMiddleware, adminMiddleware, adminController.getAllUsers);
+router.route("/contacts").get(authMiddleware,adminMiddleware, adminController.getAllContacts);
 
 module.exports = router; 
